b0390c67bb082e027a28886a25b27b7608833a3d
nikitka <nikitakitsunedev@gmail.com>
2024-02-10 21:46:22 +0300

    final sour

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./pages/index.css",
+  module: {
+    rules: [{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  mode: 'production'
+};
